daa26e5e6131fc614aba8bd7cbc6de0f18cf213b
wonderxboy <wonderxboy@gmail.com>
2015-12-25 12:34:27 +0100

    Add account section in profile view and add translations

--- JabbrClient/JabbrClient/Controllers/CLAProfileViewController.m
+++ JabbrClient/JabbrClient/Controllers/CLAProfileViewController.m
@@ -8,15 +8,18 @@
 
 #import "CLAProfileViewController.h"
 #import "Constants.h"
-#import "CLASignalRMessageClient.h"
 #import "XLForm.h"
 #import "AuthManager.h"
 
 #import "UIViewController+ECSlidingViewController.h"
 #import "SlidingViewController.h"
+#import "CLATeamViewModel.h"
+#import "CLASignalRMessageClient.h"
 #import "CLAAzureHubPushNotificationService.h"
 
 @interface CLAProfileViewController ()
+
+@property (nonatomic) BOOL rowValueSetProgramatically;
 
 @end
 
@@ -29,6 +32,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    self.navigationController.navigationBar.backItem.hidesBackButton = YES;
     self.form = [self getForm];
 }
 
@@ -38,14 +42,22 @@
 }
 
 - (void)setupNavBar {
-    self.navigationController.navigationBar.topItem.title = @"Profile";
+    self.navigationController.navigationBar.topItem.title = NSLocalizedString(@"Settings", nil);
     self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
     
-    UIBarButtonItem *menuButton = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:self action:@selector(openMenu)];
+    UIBarButtonItem *menuButton =
+    [[UIBarButtonItem alloc] initWithTitle:@""
+                                     style:UIBarButtonItemStylePlain
+                                    target:self
+                                    action:@selector(openMenu)];
     [menuButton setImage:[Constants menuIconImage]];
     self.navigationItem.leftBarButtonItem = menuButton;
     
-    UIBarButtonItem *signoutButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Sign Out", nil) style:UIBarButtonItemStylePlain target:self action:@selector(signout)];
+    UIBarButtonItem *signoutButton =
+    [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Sign Out", nil)
+                                     style:UIBarButtonItemStylePlain
+                                    target:self
+                                    action:@selector(signout)];
     self.navigationItem.rightBarButtonItem = signoutButton;
 }
 
@@ -75,21 +87,45 @@
 - (XLFormDescriptor *)getForm {
     XLFormDescriptor *form = [XLFormDescriptor
                               formDescriptorWithTitle:NSLocalizedString(@"Settings", nil)];
+    [form addFormSection:[self getAccountSection]];
     [form addFormSection:[self getTeamSection]];
     return form;
+}
+
+- (XLFormSectionDescriptor *)getAccountSection {
+    XLFormSectionDescriptor *section = [XLFormSectionDescriptor
+                                        formSectionWithTitle:NSLocalizedString(@"Account", nil)];
+    
+    XLFormRowDescriptor *username =
+    [XLFormRowDescriptor formRowDescriptorWithTag:@"Username"
+                                          rowType:XLFormRowDescriptorTypeInfo
+                                            title:NSLocalizedString(@"Username", nil)];
+    username.value = [[AuthManager sharedInstance] getUsername];
+    [section addFormRow:username];
+    
+    XLFormRowDescriptor *email =
+    [XLFormRowDescriptor formRowDescriptorWithTag:@"Email"
+                                          rowType:XLFormRowDescriptorTypeInfo
+                                            title:NSLocalizedString(@"Email", nil)];
+    //TODO: get email
+    email.value = @"abc@gmail.com";
+    [section addFormRow:email];
+    
+    return section;
 }
 
 - (XLFormSectionDescriptor *)getTeamSection {
     XLFormSectionDescriptor *section = [XLFormSectionDescriptor
                                         formSectionWithTitle:NSLocalizedString(@"Team", nil)];
     
+    NSArray<CLATeamViewModel *> *teams = [[CLASignalRMessageClient sharedInstance].dataRepository getTeams];
+    
     XLFormRowDescriptor *row =
     [XLFormRowDescriptor formRowDescriptorWithTag:@"TeamSelector"
                                           rowType:XLFormRowDescriptorTypeSelectorActionSheet
-                                            title:NSLocalizedString(@"Team", nil)];
+                                            title:NSLocalizedString(@"Switch Team", nil)];
     
     NSMutableArray *options = [NSMutableArray array];
-    NSArray<CLATeamViewModel *> *teams = [[CLASignalRMessageClient sharedInstance].dataRepository getTeams];
     
     for(CLATeamViewModel *teamViewModel in teams) {
         if (teamViewModel.team.name != nil) {
@@ -99,11 +135,30 @@
         if ([teamViewModel.team.name isEqualToString:[[AuthManager sharedInstance] getTeamName]]) {
             row.value = [XLFormOptionsObject formOptionsObjectWithValue:teamViewModel.team.name displayText:teamViewModel.team.name];
         }
+        
     }
     
     row.selectorOptions = options;
-    [section addFormRow: row];
-    
+    [section addFormRow:row];
     return section;
+}
+
+- (void)selectTeam:(XLFormRowDescriptor *)sender withValue:(id)newValue {
+    
+    if (self.rowValueSetProgramatically != NO) {
+        return;
+    }
+    
+    if (![newValue  isEqual: @1]) {
+        self.rowValueSetProgramatically = YES;
+        sender.value = @1;
+    }
+    else {
+        for(XLFormRowDescriptor *row in sender.sectionDescriptor.formRows) {
+            if (row.tag != sender.tag) {
+                row.value = @0;
+            }
+        }
+    }
 }
 @end
